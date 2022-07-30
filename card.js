a4943a44dc9c4cbcaaa98db550710e1cdc73ddb1
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-07-29 22:27:26 -0400

    created functions for card javascript file

---
+++ card.js
@@ -0,0 +1,53 @@
+function gernerateManagerCard (name,id,email,officeNumber) 
+{ 
+    return `
+    <div class="card p-3 m-3" style="width: 18rem;">
+                    
+    <div class="card-body">
+      <h5 class="card-title">${name}</h5>
+      <p class="card-text"> Manager </p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${id}</li>
+      <li class="list-group-item">Email: ${email}</li>
+      <li class="list-group-item">Office Number: ${officeNumber}</li>
+    </ul>
+    
+  </div>`
+}
+
+function gernerateEngineerCard (name,id,email,gitHub)
+{
+    return `
+    <div class="card p-3 m-3" style="width: 18rem;">
+                    
+    <div class="card-body">
+      <h5 class="card-title">${name}</h5>
+      <p class="card-text"> Engineer </p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${id}</li>
+      <li class="list-group-item">Email: ${email}</li>
+      <li class="list-group-item">GitHub: ${gitHub}</li>
+    </ul>
+    
+  </div>`
+}
+
+function gernerateInternCard (name, id, email,school)
+{
+    return `
+    <div class="card p-3 m-3" style="width: 18rem;">
+                    
+    <div class="card-body">
+      <h5 class="card-title">${name}</h5>
+      <p class="card-text">Intern </p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${id}</li>
+      <li class="list-group-item">Email: ${email}</li>
+      <li class="list-group-item">School: ${school}</li>
+    </ul>
+    
+  </div>`
+}
